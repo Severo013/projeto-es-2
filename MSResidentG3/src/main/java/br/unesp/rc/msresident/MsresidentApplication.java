7c442f437ab0867c18ef5198ab52c10a61f96461
Gabriel Severo <drvaariouz@gmail.com>
2025-07-08 18:06:04 -0300

    initial commit

---
+++ MSResidentG3/src/main/java/br/unesp/rc/msresident/MsresidentApplication.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.msresident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsresidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsresidentApplication.class, args);
+	}
+
+}
---
+++ MSResidentG3/src/test/java/br/unesp/rc/msresident/MsresidentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.msresident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsresidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
